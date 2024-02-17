f07c3f6c8ec8353cff1c11178a9458d915cefa6f
summer-royal <93848318+summer-royal@users.noreply.github.com>
2024-02-17 15:36:32 -0800

    Revert "fix file names"
    This reverts commit 9b66553918cfbf7ba23349950fde6cdbadda4e07.

--- treehacks2024/pages/__init__.py
+++ treehacks2024/pages/__init__.py
@@ -2,8 +2,8 @@ from .registration import registration
 from .login import login
 from .index import index
 
-from .view_dnr import dnr_eol
+from .dnr_eol import dnr_eol
 from .schedule import schedule
 from .ot import ot
-from .dnr_eol import view_dnr
+from .view_dnr import view_dnr
 from .will import will
