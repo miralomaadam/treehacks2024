ef1f60cb08c6cafa89e0180442bae32ec7bd3b42
summer-royal <93848318+summer-royal@users.noreply.github.com>
2024-02-18 00:32:46 -0800

    update index.py on main to reflect aesthetics branch

--- treehacks2024/pages/index.py
+++ treehacks2024/pages/index.py
@@ -2,7 +2,6 @@ from treehacks2024 import styles
 import reflex as rx
 from .login import require_login
 from ..state import State
-from ..components import navbar
 
 
 @rx.page(route="/", title="Home", image="/github.svg")
@@ -57,18 +56,6 @@ def index() -> rx.Component:
     layout = rx.chakra.vstack(top_image, rx.chakra.hstack(sidebar, main_content, direction="row", height="100vh", background_color="#dad6d2"))
 
     return rx.fragment(
+        layout,
         rx.chakra.color_mode_button(rx.chakra.color_mode_icon(), float="right"),
-        rx.vstack(
-            rx.heading("Welcome!", font_size="2em"),
-            rx.spacer(),
-            rx.link(rx.button("Schedule DNR appointment"), href="/schedule", high_contrast=True),
-            rx.link(rx.button("Answer Questions About Yoru Preferred Medical Care"), href="/dnr_eol", high_contrast=True),
-            rx.link(rx.button("View DNR"), href="/view_dnr", high_contrast=True),
-            rx.link(rx.button("Find OT interventions"), href="/ot", high_contrast=True),
-            rx.link(rx.button("Will"), href="/will", high_contrast=True),
-            rx.button("Logout", on_click=State.do_logout, float="right", size="4", high_contrast=True),
-            spacing="4",
-            padding_top="10%",
-            align="center",
-        ),
     )
